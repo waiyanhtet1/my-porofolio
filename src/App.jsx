c941621fad78100b8a10fe5925e66a0e192dddfd
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-24 16:25:17 +0630

    setup: project sturcuter with tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div className="text-xl text-red-400">
+      hello
+      <div className="container">
+        {/* <Lottie
+          animationData={AnimationData}
+          rendererSettings={{ className: "lottie-svg-class" }}
+        /> */}
+      </div>
+    </div>
+  );
+};
+
+export default App;
